e756457477a77505707cca35c59dc74b5251966a
Tommaso Vinci <tommaso.vinci@polytechnique.edu>
2017-03-22 11:56:02 +0100

    more usable gui

--- src/gui/holderGUI/holderGUI.cc
+++ src/gui/holderGUI/holderGUI.cc
@@ -2,12 +2,40 @@
 #include <QImageReader>
 #include <QFileDialog>
 #include <QTreeWidgetItem>
+#include "graphics/nView.h"
 
 holderGUI::holderGUI() : QMainWindow() {
 	setAttribute(Qt::WA_DeleteOnClose);
-	DEBUG("HERE");
 	setupUi(this);
 	show();
+}
+
+void holderGUI::keyPressEvent (QKeyEvent *event) {
+	QList<nPhysD*> selectedPhys;
+	for (auto& item: treeWidget->selectedItems()) {
+		nPhysD *my_phys=item->data(1,0).value<nPhysD*>();
+		if(my_phys) {
+			selectedPhys.append(my_phys);
+		}
+	}
+
+	switch (event->key()) {
+		case Qt::Key_Backspace:
+			for (auto& phys: selectedPhys)
+				phys->deleteLater();
+			break;
+		default:
+			break;
+	}
+}
+
+void holderGUI::on_treeWidget_itemDoubleClicked(QTreeWidgetItem* item, int i) {
+	on_actionViewer_triggered();
+	nPhysD *my_phys=item->data(1,0).value<nPhysD*>();
+	if (my_phys) {
+		emit nPhysDSelected(my_phys);
+	}
+
 }
 
 void holderGUI::on_treeWidget_itemPressed(QTreeWidgetItem* item, int i) {
@@ -20,11 +48,10 @@ void holderGUI::on_treeWidget_itemSelectionChanged() {
 	if (items.size()==1) {
 		nPhysD *my_phys=items.first()->data(1,0).value<nPhysD*>();
 		if(my_phys) {
-			graphicsView->showPhys(my_phys);
+			emit nPhysDSelected(my_phys);
 		}
 	}
 };
-
 
 void holderGUI::on_actionOpen_triggered() {
 	QString formats("");
@@ -41,6 +68,19 @@ void holderGUI::on_actionOpen_triggered() {
 	openFiles(fnames);
 }
 
+void holderGUI::on_actionViewer_triggered() {
+	if (physViewer.isNull()) {
+		physViewer = new QMainWindow(this);
+		nView *my_view = new nView(physViewer);
+		physViewer->setAttribute(Qt::WA_DeleteOnClose);
+
+		physViewer->setCentralWidget(my_view);
+		connect(this, SIGNAL(nPhysDSelected(nPhysD*)), my_view, SLOT(showPhys(nPhysD*)));
+		connect(this, SIGNAL(addPhys(nPhysD*)), my_view, SLOT(showPhys(nPhysD*)));
+	}
+	physViewer->show();
+}
+
 void holderGUI::openFiles(QStringList fnames) {
 	setProperty("NeuSave-fileOpen", fnames);
 	foreach (QString fname, fnames) {
@@ -49,25 +89,19 @@ void holderGUI::openFiles(QStringList fnames) {
 		item->setExpanded(true);
 		std::vector<nPhysD*> retlist = nHolder::getInstance().fileOpen(fname.toStdString());
 		for (auto& img: retlist) {
-			addPhys(img,item);
+			QTreeWidgetItem *my_item=new QTreeWidgetItem(item,QStringList(QString::fromStdString(img->getName())));
+			QVariant my_var=QVariant::fromValue(qobject_cast<nPhysD*>(img));
+			qDebug() << img << my_var;
+			my_item->setData(1,0,my_var);
+			my_item->setIcon(0,QIcon(":icons/icon.png"));
+			connect(img, SIGNAL(destroyed(QObject*)), this, SLOT(delPhys(QObject*)));
+			emit addPhys(img);
 		}
 	}
 }
 
-void holderGUI::addPhys(nPhysD* my_phys,QTreeWidgetItem *item) {
-	graphicsView->showPhys(my_phys);
-	QTreeWidgetItem *my_item=new QTreeWidgetItem(item,QStringList(QString::fromStdString(my_phys->getName())));
-	QVariant my_var=QVariant::fromValue(qobject_cast<nPhysD*>(my_phys));
-	qDebug() << my_phys << my_var;
-	my_item->setData(1,0,my_var);
-	my_item->setIcon(0,QIcon(":icons/icon.png"));
-	connect(my_phys, SIGNAL(destroyed(QObject*)), this, SLOT(delPhys(QObject*)));
-}
-
 void holderGUI::delPhys(QObject* my_obj) {
 	if (my_obj) {
-
-
 		QTreeWidgetItemIterator it(treeWidget);
 		while (*it) {
 			if ((*it)->data(1,0) == QVariant::fromValue(static_cast<nPhysD*>(my_obj))) {
--- src/gui/holderGUI/holderGUI.h
+++ src/gui/holderGUI/holderGUI.h
@@ -4,7 +4,6 @@
 #include "nHolder.h"
 #include "ui_holderGUI.h"
 
-
 class holderGUI: public QMainWindow, private Ui::holderGUI {
 	Q_OBJECT
 
@@ -12,19 +11,28 @@ public:
 	Q_INVOKABLE holderGUI();
 	~holderGUI(){
 		qDebug() << "destructor";
-		graphicsView->close();
 	};
 
 public slots:
 
 	void on_actionOpen_triggered();
+	void on_actionViewer_triggered();
 	void openFiles(QStringList fnames);
-	void addPhys(nPhysD* my_phys,QTreeWidgetItem *item);
 	void delPhys(QObject* my_phys);
 
 	void on_treeWidget_itemPressed(QTreeWidgetItem* item, int i);
 	void on_treeWidget_itemSelectionChanged();
+	void on_treeWidget_itemDoubleClicked(QTreeWidgetItem* item, int i);
 
+	void keyPressEvent (QKeyEvent *);
+
+signals:
+	void nPhysDSelected(nPhysD*);
+	void nPhysDDoubleClick(nPhysD*);
+	void addPhys(nPhysD*);
+
+private:
+	QPointer<QMainWindow> physViewer;
 };
 
 
